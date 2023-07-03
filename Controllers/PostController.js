f9ab5b744cfd3d2f2433d372e3a21965a82b4526
LeoFranklin015 <leofranklinjohn.25cs@licet.ac.in>
2023-07-03 17:15:56 +0530

    update post completed

--- Controllers/PostController.js
+++ Controllers/PostController.js
@@ -24,3 +24,21 @@ export const getPost = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+//Update the post
+
+export const updatePost = async (req, res) => {
+  const { id } = req.params;
+  const { currentUserId } = req.body;
+  try {
+    const post = await PostModel.findById(id);
+    if (post.userId === currentUserId) {
+      const updatedPost = await post.updateOne({ $set: req.body });
+      res.status(200).json("Post updated Successfully...");
+    } else {
+      res.status(403).json("You can only update your post...");
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
