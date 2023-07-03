d6c72fe6e1cedb4fa49f05734e941c093d9e28a9
LeoFranklin015 <leofranklinjohn.25cs@licet.ac.in>
2023-07-03 12:22:13 +0530

    Login done...

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -1,6 +1,9 @@
 import mongoose from "mongoose";
 import UserModel from "../Model/userModel.js";
-import bcrypt, { hash } from "bcrypt";
+import bcrypt from "bcrypt";
+
+//REGISTER
+
 export const registerUser = async (req, res) => {
   const { email, username, password, firstName, lastName } = req.body;
   const salt = await bcrypt.genSalt(5);
@@ -15,6 +18,27 @@ export const registerUser = async (req, res) => {
   try {
     await newUser.save();
     res.status(200).json(newUser);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+//LOGIN
+
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await UserModel.findOne({ email: email });
+    if (user) {
+      const validity = await bcrypt.compare(password, user.password);
+      if (validity) {
+        res.status(200).json(user);
+      } else {
+        res.status(400).json("Wrong password");
+      }
+    } else {
+      res.status(404).json("user not found..");
+    }
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
--- Routes/auth.js
+++ Routes/auth.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { registerUser } from "../Controllers/AuthController.js";
+import { registerUser, loginUser } from "../Controllers/AuthController.js";
 
 const router = express.Router();
 
 router.get("/", async (req, res) => res.send("Auth Page"));
 router.post("/register", registerUser);
+router.post("/login", loginUser);
 
 export default router;
